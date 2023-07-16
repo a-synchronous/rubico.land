9e22f8de3568ba17f64ac5ab859815b3a570f6e9
Richard <richytong@gmail.com>
2023-07-15 22:25:55 -0700

    tour loads correct initial version

--- public/react/Tour.js
+++ public/react/Tour.js
@@ -1,28 +1,24 @@
 import Layout from './Layout.js'
-import tourV2Mdast from '../mdast/tour-v2.js'
 import useRubicoVersion from './useRubicoVersion.js'
 import ReactElementFromMdast from './ReactElementFromMdast.js'
-
-tourV2Mdast.version = defaultRubicoVersion
 
 // Tour(props {}) -> ReactElement
 const Tour = ReactElement(props => {
   const [rubicoVersion] = useRubicoVersion()
-  const [tourMdast, setTourMdast] = useState(tourV2Mdast)
+  const [tourMdast, setTourMdast] = useState(null)
 
   useEffect(function updateTourMdast() {
-    if (tourMdast.version != rubicoVersion) {
-      import(`../mdast/tour-${rubicoVersion}.js`).then(module => {
-        const mdast = module.default
-        mdast.version = rubicoVersion
-        setTourMdast(mdast)
-      })
-    }
+    import(`../mdast/tour-${rubicoVersion}.js`).then(module => {
+      const mdast = module.default
+      setTourMdast(mdast)
+    })
   }, [rubicoVersion])
 
   return Layout(props, [
     Div({ id: 'tour' }, [
-      ReactElementFromMdast({ mdast: tourMdast })
+      tourMdast == null ? [] : [
+        ReactElementFromMdast({ mdast: tourMdast })
+      ]
     ]),
   ])
 })
